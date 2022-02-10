76f91df30cc02de9aa564126b173be1cc44747f2
Dez <dezareabryan@gmail.com>
2022-02-10 00:14:12 -0500

    Thus far I have completed the first 8 problems.

---
+++ Homework1.py
@@ -0,0 +1,122 @@
+# # Small Question 1. Hello You!
+# name = input('What is your name? ')
+# greeting = f'Hello, {name}!'
+# print(greeting)
+
+
+
+# # Small Question 2. HELLO YOU!
+# name = input('WHAT IS YOUR NAME? ')
+# length = len(name)
+# greeting = f'hello, {name}!\nyour name has {length} letters in it! Awesome!'
+# print(greeting.upper())
+
+
+
+# Small Question 3: MADLIB
+# greeting = "Please fill in the blanks below:\n_____(name)_____'s favorite subject in school is _____(subject)_____"
+# print(greeting)
+# name = input('What is the name you would like to use? ')
+# subject = input('What is the subject you would like to use? ')
+# final_greeting = f'{name}\'s favorite subject in school is {subject}.'
+# print(final_greeting)
+
+
+# # Small Question 4. Days of the Week
+# day = int(input('Day (0-6)? '))
+
+# print(day)
+
+# if day==0:
+#     print('Sunday')
+# elif day==1:
+#     print('Monday')
+# elif day==2:
+#     print('Tuesday')
+# elif day==3:
+#     print('Wednesday')
+# elif day==4:
+#     print('Thursday')
+# elif day==5:
+#     print('Friday')
+# elif day==6:
+#     print('Saturday')
+
+
+
+# # Small Question 5. Work or Sleep In?
+
+# day = int(input('Day (0-6)? '))
+
+# print(day)
+
+# if day==0:
+#     print('Sleep in')
+# elif day==1:
+#     print('Go to work')
+# elif day==2:
+#     print('Go to work')
+# elif day==3:
+#     print('Go to work')
+# elif day==4:
+#     print('Go to work')
+# elif day==5:
+#     print('Go to work')
+# elif day==6:
+#     print('Sleep in')
+
+
+
+# Small Question 6. Celsius to Fahrenheit
+
+# C = float(input("Enter temperature in Celsius "))
+# F = (C * 9/5) + 32;
+# print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(C, F))
+
+
+
+# Small Question 7. Looping from 1 to 10
+
+# a = 1
+# while a <= 10:
+#     print(a)
+#     a += 1
+
+
+
+# Small Question 8. n to m
+# MIN = int(input('What number would you like to start the sequence? '))
+
+# MAX = int(input('What number would you like to end the sequence? '))
+
+# start = MIN 
+# stop = MAX
+
+# while MIN <= MAX:
+#     print(MIN)
+#     MIN += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hello_world.py
@@ -0,0 +1,198 @@
+# print("hello world")
+
+# Hello World: my first python program
+# print("Hello World") # <--- Look! I did it!
+# friendly_greeting = "Hello World"
+# print(friendly_greeting)
+
+# Happy_Birthday = """Happy Birthday to you!
+# Happy birthday to you!
+# Happy Birthday dear Mamaw
+# Happy birthday to you!
+# I love you!
+# """
+# print(Happy_Birthday)
+
+# Birthday = "I love chocolate pie"
+# print(Birthday)
+
+# friendly_greeting = "Hello"
+# print(friendly_greeting + " " + "World")
+
+# special_characters = "This\tit\ttabbed!\n\n\nAnd has line breaks!"
+# print(special_characters)
+
+# name_of_user = input("What is your name? ")
+# print("Hello %s, it is very nice to meet you!" % name_of_user)
+
+# name_of_user = input("What is your name? ")
+# name_of_friend = input("What is your friend's name? ")
+# print("Hello %s, it is very nice to meet you and your friend %s!" % (name_of_user, name_of_friend))
+
+# greeting = "Hello %s, it is very nice to meet you and your friend %s!"
+
+# name_of_user = input("What is your name? ")
+# lenth_of_name = len(name_of_user)
+# if length_of_name > 0:
+#     name_of_friend = input("What is your friend's name? ")
+#     print(greeting % (name_of_user, name_of_friend))
+# else:
+#     print("OK, I'll ask again some other time.")
+
+
+
+# greeting = "Hello %s, it is very nice to meet you and your friend %s!"
+
+
+
+
+# name_of_user = input("What is your name? ")
+
+# length_of_name = len(name_of_user)
+
+# if length_of_name > 0:
+
+#     name_of_friend = input("What is your friend's name? ")
+#     length_of_friend_name = len(name_of_friend)
+#     if length_of_friend_name > 0:
+#       print(greeting % (name_of_user, name_of_friend))
+
+# else:
+
+#     print("OK, I'll ask again some other time.")
+
+
+# greeting = "Hello %s, it is very nice to meet you and your friend %s!"
+
+
+
+
+# name_of_user = input("What is your name? ")
+
+# length_of_name = len(name_of_user)
+
+# if length_of_name > 10:
+
+#     print("That's a really long name!")
+
+# elif length_of_name > 0:
+
+#     name_of_friend = input("What is your friend's name? ")
+
+#     length_of_friend_name = len(name_of_friend)
+
+#     if length_of_friend_name > 0:
+
+#         print(greeting % (name_of_user, name_of_friend))
+
+# # else:
+
+# #     print("OK, I'll ask again some other time.")
+
+
+
+
+# # False and print('hey')
+
+# # # Does not run the print function
+
+
+
+
+# # True and print('hey')
+
+# # # Prints "hey"
+
+# False or print('hey')
+
+# # Prints "hey"
+
+
+
+
+# True or print('hey')
+
+# # Does not run the print function
+
+
+# donuts_consumed = 0
+
+# print("You have eaten %d donuts." % donuts_consumed)
+
+# donuts_consumed = donuts_consumed + 1
+
+# print("You have eaten %d donuts." % donuts_consumed)
+
+# donuts_consumed = donuts_consumed + 1
+
+# print("You have eaten %d donuts." % donuts_consumed)
+
+# donuts_consumed = donuts_consumed + 1
+
+# print("You have eaten %d donuts." % donuts_consumed)
+
+# donuts_consumed = 0
+
+# print("You have eaten %d donuts." % donuts_consumed)
+
+# donuts_consumed += 1
+
+# print("You have eaten %d donuts." % donuts_consumed)
+
+# donuts_consumed += 1
+
+# print("You have eaten %d donuts." % donuts_consumed)
+
+# donuts_consumed += 1
+
+# print("You have eaten %d donuts." % donuts_consumed)
+
+# donuts_consumed = 0
+
+# while (donuts_consumed < 4):
+
+#     print("You have eaten %d donuts." % donuts_consumed)
+
+#     donuts_consumed = donuts_consumed + 1
+
+# while (True):
+    
+    # print("Everything is awesome. Everything is cool when you're part of a team.") #this is an example of an infinity loop. To get out of this, hit ctrl c.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

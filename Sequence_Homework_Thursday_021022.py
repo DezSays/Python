af1c99e9df68df2b328df480a3410499b41698c9
Dez <dezareabryan@gmail.com>
2022-02-15 09:31:26 -0500

    Homework complete, technically, but for three and four I need more practice because I did not answer them in the way I should have.

--- Sequence_Homework_Thursday_021022.py
+++ Sequence_Homework_Thursday_021022.py
@@ -2,10 +2,13 @@
 
 # number = int(input("Enter the number you would like to print the multiplication table of: "))    
 # for v in range(1,11):
-#     print(number,'*',v,'=',v*number)
+#     print(number,'*',v,'=',v*number) <- this will print one multiplication table at a time
 
 
-
+# # to print out a multiplication table:
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(f"{i} * {j} = {i*j}")
 
 
 
@@ -19,8 +22,13 @@
 # num = [1, 2, 3, 4, 9, 5]
 # print(sum(num))
 
+# another way to do this, the longer way, is using a for loop:
 
-
+# num = [1, 2, 3, 4, 5]
+# sum = 0
+# for x in num:
+#     sum+=x
+# print(sum)
 
 
 
@@ -34,7 +42,19 @@
 
 # print("The largest number in this list is: ",max(num))
 
+# The way it needs to be done using a for loop is below:
 
+# using for loop
+
+# nums = [2, 3, 5, 7, 11, 13, 17]
+
+# max = 0
+
+# for x in nums:
+#     if(max < x):
+#         max = x
+
+# print(max)
 
 
 
@@ -50,8 +70,15 @@
 # print("The smallest number in this list it: ", min(num))
 
 
+# nums = [2, 3, 5, 7, 11, 13, 1, 17]
 
+# min = nums[0]
 
+# for x in nums:
+#     if (min > x):
+#         min = x
+
+# print(min)
 
 
 
@@ -133,12 +160,64 @@
 # txt = "This is the final small problem for the sequence homework " [::-1]
 # print(txt)
 
+# txt = "This is the final small problem for the sequence homework "
+# reverse = ""
+# for char in txt:
+#     reverse = char + reverse
+# print(reverse)
 
 
 
 
+# Medium Question 1. Multiply Vectors
 
 
+
+# a_vector = [2, 4, 5] 
+# b_vector = [2, 3, 6]
+# c_vector = []
+
+# for i in range(0, len(b_vector)):
+#     c_vector.append(a_vector[i] * b_vector[i])
+# print(c_vector)
+
+
+
+
+# Medium Question 2: Matrix Addition
+
+# a = [2, -2]
+# b = [5, 3]
+# c = []
+
+# for i in range(0, len(b)):
+#     c.append(a[i] + b[i])
+# print(c)
+# 
+# 
+# Above is the basic addition of two matrices, below is how you would configur addition using a nested loop with multiple matrices 
+
+
+# a = [[1, 2], [3, 4]]
+# b = [[5, 6], [7, 8]]
+# c = []
+# for i in range(0, 2):
+#     for x in range(0, 2):
+#         c.append(a[i][x] + b[i][x])
+# print(c)
+
+
+# Medium Question 3: Matrix Addition II
+
+
+# a = [[1, 2, 3], [9, 10, 11], [16, 17, 18]]
+# b = [[5, 6, 7], [13, 14, 15], [19, 20, 21]]
+# c = []
+# for i in range(0, len(a)):
+#     for x in range(0, len(b)):
+#         c.append(a[i][x] + b[i][x])
+        
+# print(c)
 
 
 
